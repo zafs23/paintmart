52efcfcb404eb71b7602ff5a77b79faceb37fbcf
zafs23 <sajiazafreen@u.boisestate.edu>
2022-05-18 16:17:50 -0700

    implement navigate through different component

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { AccountService } from '../_services/account.service';
 import {User} from '../_models/user';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -11,14 +12,14 @@ import {User} from '../_models/user';
 export class NavComponent implements OnInit {
   model: any = {}
 
-  constructor(public accountService: AccountService) { }
+  constructor(public accountService: AccountService, private router: Router ) { }
 
   ngOnInit(): void {
   }
 
   login(){
     this.accountService.login(this.model).subscribe(response =>{
-      console.log(response);
+      this.router.navigateByUrl('/members');
     }, error =>{
       console.log(error);
     })
@@ -26,6 +27,7 @@ export class NavComponent implements OnInit {
 
   logout(){
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 
 }

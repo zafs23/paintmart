9d66c1875c88a6de403a536395b8a2a87744203a
zafs23 <sajiazafreen@u.boisestate.edu>
2022-05-18 18:39:31 -0700

    implement dummy routing

---
+++ client/src/app/_guards/auth.guard.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { AccountService } from '../_services/account.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor (private accountService: AccountService, private toastr: ToastrService) {}
+
+  canActivate(): Observable<boolean> {
+    return this.accountService.currUser$.pipe(
+      map(user => {
+        if(user) return true; /** this true is the observale true */
+        this.toastr.error('Register first!')
+      })
+    )
+  }
+  
+}

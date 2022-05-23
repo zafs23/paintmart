286317e0842f712958e2dc3999450e9e52a81211
zafs23 <sajiazafreen@u.boisestate.edu>
2022-05-22 23:46:58 -0700

    extend member detail view

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -18,7 +18,7 @@ const routes: Routes = [
     canActivate: [AuthGuard],
     children: [
       {path: 'members', component: MemberListComponent},
-      {path: 'members/:username', component: MemberDetailComponent},
+      {path: 'members/:userName', component: MemberDetailComponent},
       {path: 'lists', component: ListsComponent},
       {path: 'messages', component: MessagesComponent},
     ]

76c7fc17d07ad945133d0da356bcf9d572ee85b2
zafs23 <sajiazafreen@u.boisestate.edu>
2022-05-29 08:14:58 -0700

    implement member detail

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Member } from 'src/app/_models/member';
 import { MembersService } from 'src/app/_services/members.service';
 import { ActivatedRoute } from '@angular/router';
+import { NgxGalleryAnimation, NgxGalleryImage, NgxGalleryOptions } from '@kolkov/ngx-gallery';
 
 
 
@@ -13,11 +14,36 @@ import { ActivatedRoute } from '@angular/router';
 export class MemberDetailComponent implements OnInit {
 
   member: Member;
+  gallaryOptions: NgxGalleryOptions[];
+  gallaryImages: NgxGalleryImage[];
 
   constructor(private memberService: MembersService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.loadMember();
+    this.gallaryOptions = [
+      {
+        width: '500px',
+        height: '500px',
+        imagePercent: 100,
+        thumbnailsColumns:4,
+        imageAnimation: NgxGalleryAnimation.Slide,
+        preview: false
+      }
+    ]
+    this.gallaryImages = this.getImages();
+  }
+
+  getImages(): NgxGalleryImage[] {
+    const imageUrls = [];
+    for (const painting of this.member.paintings) {
+      imageUrls.push({
+        small: painting?.url,
+        medium: painting?.url,
+        big: painting?.url
+      })
+    }
+    return imageUrls;
   }
 
   loadMember(){
